fe00bf4fe70cf1452c1c6edae0c91ac09e84fefe
viest <dev@service.viest.me>
2018-07-16 09:16:16 +0800

    Test_output_buffering

--- main/main.c
+++ main/main.c
@@ -1762,26 +1762,27 @@ int php_request_startup(void)
 		PG(connection_status) = PHP_CONNECTION_NORMAL;
 		PG(in_user_include) = 0;
 
-		static const char *output_default_value_sapis[] = {
-			"apache",
-			"fastcgi",
-			"cgi-fcgi",
-			"fpm-fcgi",
-			"apache2handler",
-			"litespeed",
-			"uwsgi"
-		};
+//		static const char *output_default_value_sapis[] = {
+//			"apache",
+//			"fastcgi",
+//			"cgi-fcgi",
+//			"fpm-fcgi",
+//			"apache2handler",
+//			"litespeed",
+//			"uwsgi",
+//			NULL
+//		};
 
-		const char **sapi_name;
+//		const char **sapi_name;
 
-		if (sapi_module.name) {
-			for (sapi_name = output_default_value_sapis; *sapi_name; sapi_name++) {
-				if (strcmp(sapi_module.name, *sapi_name) == 0 && PG(output_buffering) < 1) {
-					PG(output_buffering) = 4096;
-					break;
-				}
-			}
-		}
+//		if (sapi_module.name) {
+//			for (sapi_name = output_default_value_sapis; *sapi_name; sapi_name++) {
+//				if (strcmp(sapi_module.name, *sapi_name) == 0 && PG(output_buffering) < 1) {
+//					PG(output_buffering) = 4096;
+//					break;
+//				}
+//			}
+//		}
 
 		zend_activate();
 		sapi_activate();
@@ -1811,7 +1812,7 @@ int php_request_startup(void)
 			ZVAL_STRING(&oh, PG(output_handler));
 			php_output_start_user(&oh, 0, PHP_OUTPUT_HANDLER_STDFLAGS);
 			zval_ptr_dtor(&oh);
-		} else if (PG(output_buffering)) {
+		} else if (PG(output_buffering) || strcmp(sapi_module.name, "cli")) {
 			php_output_start_user(NULL, PG(output_buffering) > 1 ? PG(output_buffering) : 0, PHP_OUTPUT_HANDLER_STDFLAGS);
 		} else if (PG(implicit_flush)) {
 			php_output_set_implicit_flush(1);
